9b8d57a2029369ea532351cbd935a094f0aa73c4
Victor Marcelino <victor.fmarcelino@gmail.com>
2019-02-23 10:45:08 -0300

    test_action now returns an index variable

--- eeve/eeve actions/test_action.py
+++ eeve/eeve actions/test_action.py
@@ -2,8 +2,14 @@ def init(*args, **kwargs):
     print('initialization args:', args, kwargs)
 
 
-def run(*args, **kwargs):
-    print('run args:', args, kwargs)
+def run(*args, index=0, **kwargs):
+    print(f'run args ({index}):', args, kwargs)
+    try:
+        index += 1
+    except Exception as e:
+        print(e)
+        index = 1
+    return {'index': index}
 
 
 actions = {'test action': {'init': init, 'run': run}}
