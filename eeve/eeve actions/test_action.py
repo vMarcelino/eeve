5d9674a151a47c8b915336aab62dc8e35144e3eb
Victor Marcelino <victor.fmarcelino@gmail.com>
2019-02-24 20:11:55 -0300

    test action is now equiped to handle variable references

--- eeve/eeve actions/test_action.py
+++ eeve/eeve actions/test_action.py
@@ -1,14 +1,24 @@
+from eeve.variable import Variable
+
+
 def init(*args, **kwargs):
     print('initialization args:', args, kwargs)
 
 
 def run(*args, index=0, **kwargs):
-    print(f'run args ({index}):', args, kwargs)
+    index_is_var = hasattr(index, 'value')
+    idx = index.value if index_is_var else index
+    print(f'run args ({idx}):', args, kwargs, end='    ')
     try:
-        index += 1
+        if index_is_var:
+            index.value += 1
+        else:
+            index += 1
     except Exception as e:
-        print(e)
+        print(e, end='')
         index = 1
+
+    print()
     return {'index': index}
 
 
