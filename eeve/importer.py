457c732ac7e1d16fa23c7f5b5d88aa2f27df61f8
Victor Marcelino <victor.fmarcelino@gmail.com>
2019-05-06 11:31:58 -0300

    added exception printing to importer and added .vscode to exclusion list

--- eeve/importer.py
+++ eeve/importer.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import importlib
+import travel_backpack
 
 
 def import_from_folder(folder):
@@ -10,18 +11,22 @@ def import_from_folder(folder):
         sys.path.insert(0, folder)
 
     for file_obj in os.listdir(folder):
-        if file_obj not in ['.', '..', '__pycache__']:
-            print('inspecting', file_obj)
-            file_obj = os.path.join(folder, file_obj)
-            if os.path.isfile(file_obj):
-                module_name = os.path.splitext(os.path.basename(file_obj))[0]
-                #module_name = os.path.basename(file_obj)
-                print('==> importing', module_name)
-                imported_files.append(importlib.import_module(module_name))
+        try:
+            if file_obj not in ['.', '..', '__pycache__', '.vscode']:
+                print('inspecting', file_obj)
+                file_obj = os.path.join(folder, file_obj)
+                if os.path.isfile(file_obj):
+                    module_name = os.path.splitext(os.path.basename(file_obj))[0]
+                    #module_name = os.path.basename(file_obj)
+                    print('==> importing', module_name)
+                    imported_files.append(importlib.import_module(module_name))
 
-            elif os.path.isdir(file_obj):
-                module_name = os.path.basename(file_obj)
-                print('--> importing', module_name)
-                imported_files.append(importlib.import_module(module_name))
+                elif os.path.isdir(file_obj):
+                    module_name = os.path.basename(file_obj)
+                    print('--> importing', module_name)
+                    imported_files.append(importlib.import_module(module_name))
+
+        except Exception as ex:
+            print(travel_backpack.format_exception_string(ex))
 
     return imported_files
